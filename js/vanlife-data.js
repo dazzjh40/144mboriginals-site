6df6fc09a1b49d3516dafa0889dda920a75faa47
dazzjh40 <86307596+dazzjh40@users.noreply.github.com>
2026-03-15 20:38:03 +0000

    vanlife-data.js

---
+++ js/vanlife-data.js
@@ -0,0 +1,22 @@
+const VANLIFE_DATA = {
+    designLinks: {
+        eventually: "https://www.etsy.com/uk/listing/4434699070/van-life-eventually-hoodie-funny",
+        level: "https://www.etsy.com/uk/listing/4467832679/funny-vanlife-t-shirt-vw-camper",
+        wildbeavers: "https://www.etsy.com/uk/listing/4461055849/vanlife-wild-beavers-t-shirt-funny",
+        schematic: "https://www.etsy.com/uk/listing/4467816668/vanlife-vw-technical-schematic-mug"
+    },
+
+    designNames: {
+        eventually: "Van Life... Eventually",
+        level: "Always on the Level",
+        wildbeavers: "Vanlife Wild Beavers",
+        schematic: "VW Technical Schematic"
+    },
+
+    imageFiles: [
+        "vanlife_eventually_white_hoodie_pose.webp",
+        "vanlife_level_sand_tee_flat.webp",
+        "vanlife_wildbeavers_green_tee_pose.webp",
+        "vanlife_schematic_white_mug_flat.webp"
+    ]
+};
