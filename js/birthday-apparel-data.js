c2c6665c9f3f31a6fa54284b0edc1c53bab47421
dazzjh40 <86307596+dazzjh40@users.noreply.github.com>
2026-03-15 15:22:00 +0000

    birthday-apparel-data.js

---
+++ js/birthday-apparel-data.js
@@ -0,0 +1,27 @@
+const BIRTHDAY_APPAREL_DATA = {
+    yearLinks: {
+        1976: "https://www.etsy.com/your-link-1976",
+        1984: "https://www.etsy.com/listing/4468286122",
+        1989: "https://www.etsy.com/uk/listing/4468282340/1989-stable-build-version-35-t-shirt",
+        1994: "https://www.etsy.com/uk/listing/4468279256/1994-stable-build-version-30-t-shirt",
+        1995: "https://www.etsy.com/uk/listing/4468262211/1995-stable-build-version-29-t-shirt",
+        1996: "https://www.etsy.com/uk/listing/4468257965/1996-birthday-shirt-version-28-stable"
+    },
+
+    yearMeta: {
+        1976: { version: "1.0", title: "Version 1.0 • Born 1976" },
+        1984: { version: "4.0", title: "Version 4.0 • Born 1984" },
+        1989: { version: "3.5", title: "Version 3.5 • Born 1989" },
+        1994: { version: "3.0", title: "Version 3.0 • Born 1994" },
+        1995: { version: "2.9", title: "Version 2.9 • Born 1995" },
+        1996: { version: "2.8", title: "Version 2.8 • Born 1996" }
+    },
+
+    imageFiles: [
+        "birthday_1976_pink_flat.webp",
+        "birthday_1984_dkblue_flat.webp",
+        "birthday_1984_paleblue_flat.webp",
+        "birthday_1994_blue_flat.webp",
+        "birthday_1996_ltgrey_flat.webp"
+    ]
+};
