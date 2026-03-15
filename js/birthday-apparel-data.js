c163c222383ab51b7845141d1b369af13831d961
dazzjh40 <86307596+dazzjh40@users.noreply.github.com>
2026-03-15 18:54:18 +0000

    birthday-apparel-data.js

--- js/birthday-apparel-data.js
+++ js/birthday-apparel-data.js
@@ -18,7 +18,8 @@ const BIRTHDAY_APPAREL_DATA = {
     },
 
     imageFiles: [
-        "birthday_1976_ltblue_pose.webp"
+        "birthday_1976_ltblue_pose.webp",
+        "birthday_1976_green_fold.webp",
         "birthday_1976_pink_flat.webp",
         "birthday_1984_dkblue_flat.webp",
         "birthday_1984_paleblue_flat.webp",
