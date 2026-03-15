c24bda3300831c5d3b429c85462b98f277bff5a1
dazzjh40 <86307596+dazzjh40@users.noreply.github.com>
2026-03-15 20:57:48 +0000

    vanlife-data.js

--- js/vanlife-data.js
+++ js/vanlife-data.js
@@ -3,20 +3,27 @@ const VANLIFE_DATA = {
         eventually: "https://www.etsy.com/uk/listing/4434699070/van-life-eventually-hoodie-funny",
         level: "https://www.etsy.com/uk/listing/4467832679/funny-vanlife-t-shirt-vw-camper",
         wildbeavers: "https://www.etsy.com/uk/listing/4461055849/vanlife-wild-beavers-t-shirt-funny",
-        schematic: "https://www.etsy.com/uk/listing/4467816668/vanlife-vw-technical-schematic-mug"
+        schematic: "https://www.etsy.com/uk/listing/4467816668/vanlife-vw-technical-schematic-mug",
+        upsidedown_tee: "https://www.etsy.com/uk/listing/4471866505/feeling-fruity-upside-down-pineapple",
+        upsidedown_cushion: "https://www.etsy.com/uk/listing/4472313648/feeling-fruity-outdoor-pillow-funny"
+        
     },
 
     designNames: {
         eventually: "Van Life... Eventually",
         level: "Always on the Level",
         wildbeavers: "Vanlife Wild Beavers",
-        schematic: "VW Technical Schematic"
+        schematic: "VW Technical Schematic",
+        upsidedown_tee: "Cheeky Fun Tee IYKYK",
+        upsidedown_cushion: "Cheeky Fun Cushion IYKYK",
     },
 
     imageFiles: [
         "vanlife_eventually_white_hoodie_pose.webp",
         "vanlife_level_sand_tee_flat.webp",
         "vanlife_wildbeavers_green_tee_pose.webp",
-        "vanlife_schematic_white_mug_flat.webp"
+        "vanlife_schematic_white_mug_flat.webp",
+        "vanlife_fruit_grey_cushion_flat.webp",
+        "vanlife_fruit_grey_tee_flat.webp",
     ]
 };
