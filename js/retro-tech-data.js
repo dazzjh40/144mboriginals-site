2bd78fd8f44f6d7c16cfb0f5e58f653f143f8238
dazzjh40 <86307596+dazzjh40@users.noreply.github.com>
2026-03-16 21:09:04 +0000

    retro-tech-data.js

---
+++ js/retro-tech-data.js
@@ -0,0 +1,43 @@
+const RETRO_TECH_DATA = {
+
+designNames: {
+lowpower: "Low Power Minimalist Tech",
+fullpower: "Full Power Restored",
+lowbattery: "Low Battery Power Saving Mode",
+floppy144: "1.44MB Retro Floppy Disk",
+retrobadge: "Retro Computer Nerd Badge"
+},
+
+designLinks: {
+lowpower: "YOUR_ETSY_LINK_HERE",
+fullpower: "YOUR_ETSY_LINK_HERE",
+lowbattery: "YOUR_ETSY_LINK_HERE",
+floppy144: "YOUR_ETSY_LINK_HERE",
+retrobadge: "YOUR_ETSY_LINK_HERE"
+},
+
+imageFiles: [
+
+/* LOW POWER */
+"lowpower_black_flat.webp",
+"lowpower_black_pose.webp",
+
+/* FULL POWER */
+"fullpower_green_flat.webp",
+"fullpower_green_pose.webp",
+
+/* LOW BATTERY */
+"lowbattery_blue_flat.webp",
+"lowbattery_blue_pose.webp",
+
+/* FLOPPY */
+"floppy144_navy_flat.webp",
+"floppy144_black_pose.webp",
+
+/* RETRO BADGE */
+"retrobadge_grey_flat.webp",
+"retrobadge_grey_pose.webp"
+
+]
+
+};
