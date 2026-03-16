f2d7e3b626d28bc0e4d325434647fa1fa89bf597
dazzjh40 <86307596+dazzjh40@users.noreply.github.com>
2026-03-16 19:53:39 +0000

    birthday-version-data.js

---
+++ js/birthday-version-data.js
@@ -0,0 +1,20 @@
+const birthdayVersionData = {
+  1976: {
+    version: "5.0",
+    image: "images/small_img/birthday/birthday_1976_pink_flat.webp",
+    etsyUrl: "YOUR_ETSY_LINK_HERE",
+    title: "Version 5.0 Born 1976"
+  },
+  1984: {
+    version: "4.0",
+    image: "images/small_img/birthday/birthday_1984_dkblue_flat.webp",
+    etsyUrl: "YOUR_ETSY_LINK_HERE",
+    title: "Version 4.0 Born 1984"
+  },
+  1994: {
+    version: "3.0",
+    image: "images/small_img/birthday/birthday_1994_blue_flat.webp",
+    etsyUrl: "YOUR_ETSY_LINK_HERE",
+    title: "Version 3.0 Born 1994"
+  }
+};
